c6c299be13ec9884e789ab4c9bc157954c958ebc
888Greys <toxicgreys001@gmail.com>
2024-05-22 11:45:38 +0300

    Update

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,41 +1,53 @@
 #!/usr/bin/env python3
-""" 0-basic_cache.py """
+"""Create a class BasicCache that inherits from
+BaseCaching and is a caching system:
 
+You must use self.cache_data - dictionary from
+the parent class BaseCaching
+This caching system doesn’t have limit
+def put(self, key, item):
+Must assign to the dictionary self.cache_data the
+item value for the key key.
+If key or item is None, this method should not do
+anything.
+def get(self, key):
+Must return the value in self.cache_data linked
+to key.
+If key is None or if the key doesn’t exist in
+self.cache_data, return None.
+"""
 
-from base_caching import BaseCaching
 
 BaseCaching = __import__('base_caching').BaseCaching
 
+
 class BasicCache(BaseCaching):
-    """ BasicCache defines:
-      - caching system
+    """_summary_
     """
 
     def __init__(self):
-        """ Initiliaze
+        """_summary_
         """
         super().__init__()
 
     def put(self, key, item):
-        """ Add key/value pair to cache data.
+        """_summary_
 
-        args:
-            key: key for item
-            item: value to add to cache
+        Args:
+                key (_type_): _description_
+                item (_type_): _description_
         """
-
         if key is None or item is None:
             pass
         else:
             self.cache_data[key] = item
 
     def get(self, key):
-        """ Get value of key from cache data.
+        """return the value in self.cache_data linked to key
 
-        args:
-            keys: key to retrieve from cache data
+        Args:
+                key (_type_): _description_
         """
-
         if key is None or key not in self.cache_data.keys():
             return None
         return self.cache_data.get(key)
